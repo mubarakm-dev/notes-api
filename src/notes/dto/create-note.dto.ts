8f772e7863531fbe84f2d74e09b55674b40f85a7
Mubarak Muhammed <mubarakoladele98@gmail.com>
2026-09-19 22:06:49 +0100

    fix: update CreateNoteDTO to include IsNotEmpty validation for body

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,12 +1,11 @@
-import { IsString, MinLength } from "class-validator";
-
-
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class CreateNoteDTO {
     @IsString()
     @MinLength(5)
     title!: string;
-    
+
     @IsString()
+    @IsNotEmpty()
     body!: string;
 }
