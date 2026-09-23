49b6b57f5f7f9635cdf503eddbaabdb81c054adc
Mubarak Muhammed <mubarakoladele98@gmail.com>
2026-09-23 22:24:47 +0100

    feat: implement signup endpoint with validation in AuthController and update SignupDto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,11 +1,7 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
-import { string } from "joi";
-
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class SignupDto{
-     @IsEmail()
-    @IsString()
-    @IsNotEmpty()
+    @IsEmail()
     email!: string;
 
     @IsString()
