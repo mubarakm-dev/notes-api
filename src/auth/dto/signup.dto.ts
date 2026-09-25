6179af8820e1955cb1c80d36c7fef80c38197d23
Mubarak Muhammed <mubarakoladele98@gmail.com>
2026-09-25 18:20:01 +0100

    feat: add password validation regex to SignupDto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,12 +1,15 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { IsEmail, IsString, Matches, MinLength } from "class-validator";
 
-export class SignupDto{
+export class SignupDto {
     @IsEmail()
     email!: string;
 
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/, {
+        message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    })
     @IsString()
     @MinLength(7)
     password!: string;
 }
-   
+
 
