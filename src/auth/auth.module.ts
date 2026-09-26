a1f8421f94707324d00d5027ad16cf22153f6d1e
Mubarak Muhammed <mubarakoladele98@gmail.com>
2026-09-26 22:58:04 +0100

    feat: add JWT strategy and auth guard to enhance authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 
 @Module({
@@ -19,7 +21,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
 
 })
 export class AuthModule { }
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,9 +1,11 @@
+import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(config: ConfigService) {
         super({
@@ -13,9 +15,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         })
     }
 
-    async validate(payload: {sub: string, email: string, role:string}) {
-        
-        return {userId:payload.sub, email:payload.email, }
+    async validate(payload: { sub: string, email: string, role: string }) {
+
+        return {
+            userId: payload.sub, email: payload.email, role: payload.role
+
+        }
     }
 
 }
